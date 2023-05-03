2539791c8ea95dbc6f292ebc059db85a4250bd1c
Cauane Marques <61557199+HorsMarques@users.noreply.github.com>
2023-05-03 07:53:29 -0300

    Delete Código/projeto directory

--- Código/projeto/arquitetura/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def pagprincipal(request):
-    return render(request, 'arquitetura/pagprincipal.html')
-
-def base(request):
-    return render(request, 'templates/base.html')
-
