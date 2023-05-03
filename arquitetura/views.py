e9787c9a713072eb6b1f6ecf0bf7665849a01607
Cauane Marques <61557199+HorsMarques@users.noreply.github.com>
2023-05-03 08:08:24 -0300

    Delete arquitetura directory

--- arquitetura/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-def pagprincipal(request):
-    return render(request, 'arquitetura/pagprincipal.html')
-
-def base(request):
-    return render(request, 'templates/base.html')
-
