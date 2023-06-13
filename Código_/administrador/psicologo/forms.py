871beb1932c33aef38ef8bc68592e43b6f0d9531
Cauane Marques <61557199+HorsMarques@users.noreply.github.com>
2023-06-13 18:11:40 -0300

    Delete Código_/administrador directory

--- Código_/administrador/psicologo/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Psicologo
-
-class PsicologoForm(forms.ModelForm):
-    class Meta:
-        model = Psicologo
-        fields = ['nome', 'senha', 'email', 'crp', 'data_nascimento']
--- Código_/administrador/usuario/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from.models import usuario
-
-class usuarioForm(forms.ModelForm):
-    class Meta: 
-        model= usuario
-        fields = ['nome', 'email', 'data_nascimento', 'senha']
